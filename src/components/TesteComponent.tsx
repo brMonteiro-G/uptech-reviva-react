c84675259a97e78f2d2df6a31cd126d3da93b2e5
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-06-01 16:49:35 -0300

    chore():setup project

---
+++ src/components/TesteComponent.tsx
@@ -0,0 +1,8 @@
+import style from "./Teste.module.scss";
+
+function Teste(){
+    return(
+        <span className={style["teste__react--component"]} >Esse componente está funcionando</span>
+    )
+}
+export default Teste
