f9be5aa45f639457c32dc902bba14f7f36b0d20a
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-06-15 18:33:26 -0300

    fix():fixing eslint warning

--- src/pages/components/windowShopper/Products.tsx
+++ src/pages/components/windowShopper/Products.tsx
@@ -1,10 +1,10 @@
 
-import style from "./Products.module.scss";
-import Inventory from "../../inventory";
-import { ReactElement, useState } from "react";
-import { Button } from "../Button/Button";
-import { storageState } from "../../state/atoms/storageState";
-import { useRecoilValue } from "recoil";
+import style from './Products.module.scss';
+import Inventory from '../../inventory';
+import { ReactElement, useState } from 'react';
+import { Button } from '../Button/Button';
+import { storageState } from '../../state/atoms/storageState';
+import { useRecoilValue } from 'recoil';
 //refatorar inventario 
 
 export interface Items {
@@ -36,13 +36,13 @@ export default function Products(props: Props) {
       <div className={`${style.boxModel}`} data-id={index}>
 
         <a href="#">
-          <div className={style["div-image-buttons"]}>
-            <img className={style["img-prod"]} src={process.env.PUBLIC_URL + element.images[0].url} alt={element.images[0].description} />
+          <div className={style['div-image-buttons']}>
+            <img className={style['img-prod']} src={process.env.PUBLIC_URL + element.images[0].url} alt={element.images[0].description} />
           </div>
         </a>
         <div className={`${style.product__view}`}>
-          <p className={style["product__view--name"]}>{element.name} </p>
-          <p className={style["product__view--price"]}>R$ {element.price.toFixed(2)} </p>
+          <p className={style['product__view--name']}>{element.name} </p>
+          <p className={style['product__view--price']}>R$ {element.price.toFixed(2)} </p>
         </div>
 
         <Button
@@ -57,12 +57,12 @@ export default function Products(props: Props) {
       {
         productsContainer.map((element, index) => {
                 
-          if (props.texto.includes("1")) {
+          if (props.texto.includes('1')) {
                         
             if (index <= 3) {
               return (template(element, index));
             }
-          } if (props.texto.includes("2")) {
+          } if (props.texto.includes('2')) {
             if (index >= 4) {
               return (template(element, index));
             }
