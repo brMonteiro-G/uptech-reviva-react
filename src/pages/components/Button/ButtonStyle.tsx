62ee8c8e535afda0bcfecef5c96519868c0cadee
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-06-20 15:31:21 -0300

    fix(): fixing git error

--- src/pages/components/Button/ButtonStyle.tsx
+++ src/pages/components/Button/ButtonStyle.tsx
@@ -1,49 +1,50 @@
 import styled from 'styled-components';
 import { tertiary_color } from 'Variables';
 
-
 export const DivBackgroundBag = styled.div`
-display: grid;
-grid-template-columns: 3fr 1fr;
-grid-template-rows: 42px;
-justify-content: center;
-width: 285px;
-border-radius: 1%;
-margin-top: 6em;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  grid-template-rows: 42px;
+  justify-content: center;
+  width: 285px;
+  border-radius: 1%;
+  margin-top: 6em;
 
-img {
+  img {
+    justify-self: center;
+    align-self: center;
+    padding: 0 2% 2% 0;
+  }
 
-  justify-self: center;
-  align-self: center;
-  padding: 0 2% 2% 0;
-}
-
-p {
-  align-self: center;
-  justify-self: flex-end;
-}
-
+  p {
+    align-self: center;
+    justify-self: flex-end;
+  }
 `;
 
 export const CartButton = styled.button`
-background-color: ${tertiary_color};
-border: none;
-font-size: 15px;
-color: white;
+  background-color: ${tertiary_color};
+  border: none;
+  font-size: 15px;
+  color: white;
 
-&:hover {
+  &:hover {
     cursor: pointer;
     color: white;
-}
-
+  }
 `;
 
 export const ShoppingCartButton = styled.button`
-background-image: url(\"../../../../public/images/shopping-bag.png\");
+  background-image: url(\"../../../../public/images/shopping-bag.png\");
   background-repeat: no-repeat;
   background-position: 50% 50%;
 `;
 
-
-
-
+export const DivButtonsDetails = styled(DivBackgroundBag)`
+position:relative;
+left:420px;
+`;
+export const CartButtonDetails = styled(CartButton)`
+border-radius:20px;
+width:500px;
+`;
